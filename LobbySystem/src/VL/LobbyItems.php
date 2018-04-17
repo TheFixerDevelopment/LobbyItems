d0c801a98e641425cbb81537754444ebe306485c
zLeqitSky <38416244+zLeqitSky@users.noreply.github.com>
2018-04-17 20:23:26 +0200

    Update Bug transfer

--- LobbySystem/src/VL/LobbyItems.php
+++ LobbySystem/src/VL/LobbyItems.php
@@ -213,8 +213,8 @@ class LobbyItems extends PluginBase implements Listener
 		$exit = Item::get(351, 1, 1);
 		$exit->setCustomName(TextFormat::RESET . TextFormat::RED . "Exit");
 
-		$Hub = Item::get(42,1, 1);
-		$Hub->setCustomName(TextFormat::RESET . TextFormat::BLUE . "Hub");
+		$MainHub = Item::get(41,1, 1);
+		$MainHub->setCustomName(TextFormat::RESET . TextFormat::BLUE . "MainHub");
 
 		$MiniGames = Item::get(322, 1, 1);
 		$MiniGames->setCustomName(TextFormat::RESET . TextFormat::GREEN . "MiniGames");
@@ -227,7 +227,7 @@ class LobbyItems extends PluginBase implements Listener
 
 		$inv->setItem(8, $exit);
 		$inv->setItem(0, $CityBuild);
-		$inv->setItem(2, $Hub);
+		$inv->setItem(2, $MainHub);
 		$inv->setItem(4, $MiniGames);
 		$inv->setItem(6, $Bald);
 
@@ -588,7 +588,7 @@ class LobbyItems extends PluginBase implements Listener
 
 		}
 		//run
-		if ($in == TextFormat::RESET . TextFormat::RED . "Hub") {
+		if ($in == TextFormat::RESET . TextFormat::RED . "MainHub") {
 			$event->getPlayer()->transfer("EnjoyTheView.tk", "19132");
 		}
 		if ($in == TextFormat::RESET . TextFormat::GREEN . "MiniGames") {
